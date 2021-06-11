fad4a1ed349fac35e083d7850dda9a1487380623
Shamilx <shamil.shamiyevx@gmail.com>
2021-06-11 18:58:10 +0400

    Lesson 3

---
+++ Lesson 3/WinFormsApp3/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    enum FunctionType
+    {
+        XETTI,
+        KVADRATIK
+    }
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+
+            FunctionType functionType = FindOutFunctionType(text);
+
+            if (functionType == FunctionType.XETTI)
+            {
+                DrawXettiFunction(text);
+            }
+            else
+            {
+                DrawKvadratikFunksiya(text);
+            }
+        }
+
+        private void DrawKvadratikFunksiya(string text)
+        {
+            
+        }
+
+        private void DrawXettiFunction(string text)
+        {
+            string problem;
+            int? x = null;
+
+            if (!text.Contains("x"))
+            {
+                MessageBox.Show("Please provide correct function!");
+                return;
+            }
+            
+            List<Point> points = new List<Point>();
+            
+            for (int i = -10; i <= 10; i++)
+            {
+                problem = text.Replace("x", i.ToString());
+
+                if (problem.Contains("y="))
+                {
+                    problem = problem.Remove(0, 2);
+                }
+
+                x = SolveProblem(problem);
+                
+                if (x.HasValue)
+                {
+                    points.Add(new Point(i,x.Value));
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (x.HasValue)
+            {
+                // Form2 form2 = new Form2(points);
+                // form2.ShowDialog();
+            }
+        }
+
+        private int? SolveProblem(string problem)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                var v = dt.Compute(problem, "");
+                return (int) v;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please provide correct function!");
+            }
+
+            return null;
+        }
+
+        private FunctionType FindOutFunctionType(string text)
+        {
+            if (text.Contains("^"))
+            {
+                return FunctionType.KVADRATIK;
+            }
+            
+            return FunctionType.XETTI;
+        }
+    }
+}
---
+++ Lesson 3/WinFormsApp3/Form2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class Form2 : Form
+    {
+        private List<Point> _points;
+        
+        public Form2(List<Point> points)
+        {
+            InitializeComponent();
+
+            _points = FindFarPoints(points);
+            this.Paint += OnPaint;
+            
+        }
+
+        private List<Point> FindFarPoints(List<Point> points)
+        {
+            double maxDistance = 0;
+            List<Point> farDistance = new List<Point>();
+            
+            for (int i = 0; i < points.Count; i++)
+            {
+                for (int j = 0; j < points.Count; j++)
+                {
+                    double distance = FindDistanceForPoints(points[i], points[j]);
+
+                    if (distance > maxDistance)
+                    {
+                        maxDistance = distance;
+                        farDistance.Clear();
+
+                        farDistance.Add(points[i]);
+                        farDistance.Add(points[j]);
+                    }
+                }
+            }
+
+            return farDistance;
+        }
+
+        private double FindDistanceForPoints(Point point, Point point1)
+        {
+            int X = point1.X - point.X;
+            int Y = point1.Y - point.Y;
+
+            int sum = Convert.ToInt32(Math.Pow(X, 2)) + Convert.ToInt32(Math.Pow(Y, 2));
+
+            return Math.Sqrt(sum);
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            
+            var x = e.Graphics;
+            
+            Pen penBlue = new Pen(Color.Blue, 10);
+            Pen penRed = new Pen(Color.Red, 10);
+
+            x.DrawLine(penBlue, 13, 220, 433, 220);
+            x.DrawLine(penRed, 220, 9, 220, 429);
+            
+            Point[] myPoints = _points.ToArray();
+            
+            myPoints[0] = new Point(myPoints[0].X * 20,myPoints[0].Y * 20);
+            myPoints[1] = new Point(myPoints[1].X * 20,myPoints[1].Y * 20);
+            
+            x.DrawLine(penRed,myPoints[0], myPoints[1]);
+        }
+    }
+}
